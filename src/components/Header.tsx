011da90599a54270386f824dcca9c8969f016adb
francisco passarelli <118229728+franciscopassarelli@users.noreply.github.com>
2025-04-04 01:41:06 -0300

    about-m

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,7 +58,7 @@ const Header: React.FC = () => {
                 Calculadora
               </a>
               <a 
-                href="/about"
+                href="/About"
                 className="py-3 px-4 text-sm font-medium hover:bg-primary/5 rounded-md transition-colors"
                 onClick={() => setIsMenuOpen(false)}
               >
